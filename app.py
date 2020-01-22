33146b11cd7af630790e1672ac05811b0cfaae66
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-01-22 09:01:51 +0200

    Create the add food form in the foods page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, request, g
 import sqlite3
 
 
@@ -60,12 +60,29 @@ def day():
     return render_template('day.html')
 
 
-@app.route('/foods')
+@app.route('/foods', methods=['GET', 'POST'])
 def foods():
     """
     Return the foods page which list all foods in the app, and add new
     food to the app.
     """
+
+    if request.method == 'POST':
+        # Getting the form data in local vars
+        name = request.form['name']
+        protein = int(request.form['protein'])
+        carbohydrates = int(request.form['carbohydrates'])
+        fat = int(request.form['fat'])
+        calories = 4 * protein + 4 * carbohydrates + 9 * fat
+        # Debugging form data in console
+        # print("name: {}, protein: {}, carbohydrates: {}, fat: {}".format(
+        #     name, protein, carbohydrates, fat))
+        # Storing the form data in the DB
+        db = get_db()
+        db.execute("""insert into food (name, protein, carbohydrates, fat, calories)
+                   values (?, ?, ?, ?, ?)""",
+                   [name, protein, carbohydrates, fat, calories])
+        db.commit()
 
     return render_template('foods.html')
 
